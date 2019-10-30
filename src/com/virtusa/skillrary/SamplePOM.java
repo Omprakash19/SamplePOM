5ed205577518ddce419b7b9e0b2348a1595f7fc5
Omprakash <Omprakash@LAPTOP-H87M2369>
2019-10-30 16:00:59 +0530

    my first  commit

--- src/com/virtusa/skillrary/SamplePOM.java
+++ src/com/virtusa/skillrary/SamplePOM.java
@@ -1,10 +1,25 @@
 package com.virtusa.skillrary;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SamplePOM {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/");
+		WebElement ele =driver.findElement(By.id("username"));
+		driver.navigate().refresh();
+		ele.sendKeys("admin");
 	}
 
 }
--- src/popup/Fileupload.java
+++ src/popup/Fileupload.java
@@ -1,9 +1,17 @@
 package popup;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Fileupload {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
 
 	}
 
